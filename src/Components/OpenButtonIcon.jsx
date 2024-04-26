61a943d484092a745da225ac41c3f83247fcd552
Calvarez <cesar34313@gmail.com>
2024-04-26 13:08:53 -0600

    Motivos de facturacion con respecto a viaje

--- src/Components/OpenButtonIcon.jsx
+++ src/Components/OpenButtonIcon.jsx
@@ -1,8 +1,13 @@
 import { RxOpenInNewWindow } from "react-icons/rx"
 import IconButton from './IconButton'
-function OpenButtonIcon({size, url}) {
+function OpenButtonIcon({size, url, emergente}) {
+    function cadenaEmergente(){
+        const screenWidth = window.screen.width;
+        const screenHeight = window.screen.height;
+        return `width=${Math.round(screenWidth*0.4)}, height=${screenHeight<1000?Math.round(screenHeight*0.6) :Math.round(screenHeight*0.457)}`
+    }
     return (
-        <IconButton icon={ <RxOpenInNewWindow size={size}/>} fn={()=>window.open(`${url}`,'_blank',)}/>
+        <IconButton icon={ <RxOpenInNewWindow size={size}/>} fn={()=>window.open(`${url}`,'_blank', emergente===true?cadenaEmergente():'')}/>
     )
 }
 
