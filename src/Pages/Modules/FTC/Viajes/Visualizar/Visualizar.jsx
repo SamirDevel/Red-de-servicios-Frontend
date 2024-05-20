86b44f2e72adc19d9daa840e0ad0411629da0ceb
Samir Priego <samir.apriego@gmail.com>
2024-05-20 15:12:59 -0600

    Atulizacion Facturacion
    Ahora facturacio utiliza las vistas generales de viajes

--- src/Pages/Modules/FTC/Viajes/Visualizar/Visualizar.jsx
+++ src/Pages/Modules/FTC/Viajes/Visualizar/Visualizar.jsx
@@ -1,57 +1,9 @@
-import {useState, useEffect} from 'react'
-import Filtros from '../../../../../Components/Viajes/Filtros'
-import Table from '../../../../../Components/Table V2'
-import { viajesFns } from '../../../../../Components/Viajes/Functions';
-import PrintButtonIcon from '../../../../../Components/PrintButtonIcon';
-import { fns } from '../../../../../Functions';
-import makePDF from '../VistaPDF.JS';
+import VisualizarComponent from '../../../../../Components/Viajes/Consultar/Visualizar'
 function Visualizar() {
-    const [viajes, setViajes] = useState([]);
-    const [objetos, setObjetos] = useState([]);
-    const heads = [
-        {text:'Imprimir', type:'string'},
-        {text:'Serie', type:'string'},
-        {text:'Folio', type:'string'},
-        {text:'Chofer', type:'string'},
-        {text:'Auxiliar', type:'string'},
-        {text:'Vehiculo', type:'string'},
-        {text:'Ruta', type:'string'},
-        {text:'Expedicion', type:'string'},
-        {text:'Usuario', type:'string'}
-    ]
-    useEffect(()=>{
-        async function getObjetos(){
-            const objetos = await viajesFns.makeObjetos(viajes,'facturacion', (viaje, toPrint)=>{
-                return  {
-                    IMPRIMIR:< PrintButtonIcon size={30} fn={()=>makePDF(toPrint)}/>,
-                    SERIE:viaje['serie']['serie'],
-                    FOLIO:viaje['folio'],
-                    CHOFER:viaje['chofer']['nombre'],
-                    AUXILIAR:viaje['auxiliar']!= null?viaje['auxiliar']['nombre']:'',
-                    VEHICULO:`${viaje['vehiculo']['codigo']}`,
-                    RUTA:viaje['nombreRuta'],
-                    EXPEDICION: fns.dateString(new Date(viaje['expedicion'])),
-                    USUARIO:viaje['nombreUsuario'],
-                }
-            })
-            setObjetos(objetos)
-        }
-        if(viajes.length>0)getObjetos();
-    },[viajes])
+    
     return(
-        <div className='flex flex-col items-center flex-grow'>
-            <br />
-            <label className='text-2xl text-cyan-800 font-extrabold'>Consulta de registros</label>
-            <br />
-            <Filtros area={'facturacion'} setDocs={setViajes}/>
-            <br />
-            <div className={`${viajes.length>0?'visble':'hidden'}`}>
-                <Table  theme='bg-blue-950 text-white' colsHeads={heads} list={objetos} manage={setObjetos}/>
-            </div>
-        </div>
+        <VisualizarComponent area={'facturacion'}/>
     )
 }
 
 export default Visualizar
-/*
-*/
